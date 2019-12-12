0d6a52a17d896d6a4d6c4f8c7eb92edb20b90ec3
Anders Kagervall <anders.kagervall@gmail.com>
2019-12-12 18:52:27 +0100

    SLUpalette can return colors without names

--- R/SLUcolors.R
+++ R/SLUcolors.R
@@ -20,57 +20,65 @@ names(palette_list) <- palette_list
 #' palette(opal)
 #
 #'
-SLUpalette <- function(palette) {
+SLUpalette <- function(palette, color_names = TRUE) {
   UseMethod("SLUpalette")
 }
 
-SLUpalette.default <- function(palette) {
+SLUpalette.default <- function(palette, color_names) {
   stop('Parameter "palette" must be character or numeric')
 }
-SLUpalette.character  <- function(palette) {
+SLUpalette.character  <- function(palette, color_names = TRUE) {
   if (!palette %in% palette_list) {
     stop('Unknown color palette "',palette, '" must be one of ', paste(palette_list, collape = " "))
   }
-  switch(palette,
-         all = c(SLUpalette("green"), SLUpalette("red"), SLUpalette("blue"),
-                 SLUpalette("yellow"), SLUpalette("grey")),
-         red = rev(rgb(red = c(251, 255, 206, 103, 80),
-                   green = c(215, 88, 0, 33, 43),
-                   blue = c(201, 93, 55, 70, 58),
-                   max = 255,
-                   names = c("Aprikos", "Korall", "Hallon",
-                             "Plommon", "Druva"))),
-         green = rev(rgb(red = c(216, 196, 80, 121, 21),
-                     green = c(237, 214, 158, 134, 71),
-                     blue = c(150, 0, 47, 60, 52),
-                     max = 255,
-                     names = c("Lindblom", "Äpple", "Klorofyll",
-                               "Oliv", "Skog"))),
-         blue = rev(rgb(red = c(185, 106, 0, 0, 0),
-                    green = c(211, 209, 176, 118, 72),
-                    blue = c(220, 227, 185, 129, 81),
-                    max = 255,
-                    names = c("Duva", "Himmel", "Turkos",
-                              "Havsvik", "Havsdjup"))),
-         yellow = rev(rgb(red = c(246, 252, 255, 153, 75),
-                      green = c(234, 227, 184, 96, 61),
-                      blue = c(97, 0, 28, 23, 42),
-                      max = 255,
-                      names = c("Solsken", "Majs", "Kåda",
-                                "Kanel", "Choklad"))),
-         grey = rev(rgb(red = c(255, 217, 187, 136, 83, 0),
-                    green = c(255, 217, 188, 139, 86, 0),
-                    blue = c(255, 214, 188, 141, 90, 0),
-                    max = 255,
-                    names = c("Snö", "Fjäder", "Glimmer", "Betong",
-                              "Titan", "Kol")))
+  pal <- switch(palette,
+                all = c(SLUpalette("green"), SLUpalette("red"), SLUpalette("blue"),
+                        SLUpalette("yellow"), SLUpalette("grey")),
+                red = rev(rgb(red = c(251, 255, 206, 103, 80),
+                              green = c(215, 88, 0, 33, 43),
+                              blue = c(201, 93, 55, 70, 58),
+                              max = 255,
+                              names = c("Aprikos", "Korall", "Hallon",
+                                        "Plommon", "Druva"))),
+                green = rev(rgb(red = c(216, 196, 80, 121, 21),
+                                green = c(237, 214, 158, 134, 71),
+                                blue = c(150, 0, 47, 60, 52),
+                                max = 255,
+                                names = c("Lindblom", "Äpple", "Klorofyll",
+                                          "Oliv", "Skog"))),
+                blue = rev(rgb(red = c(185, 106, 0, 0, 0),
+                               green = c(211, 209, 176, 118, 72),
+                               blue = c(220, 227, 185, 129, 81),
+                               max = 255,
+                               names = c("Duva", "Himmel", "Turkos",
+                                         "Havsvik", "Havsdjup"))),
+                yellow = rev(rgb(red = c(246, 252, 255, 153, 75),
+                                 green = c(234, 227, 184, 96, 61),
+                                 blue = c(97, 0, 28, 23, 42),
+                                 max = 255,
+                                 names = c("Solsken", "Majs", "Kåda",
+                                           "Kanel", "Choklad"))),
+                grey = rev(rgb(red = c(255, 217, 187, 136, 83, 0),
+                               green = c(255, 217, 188, 139, 86, 0),
+                               blue = c(255, 214, 188, 141, 90, 0),
+                               max = 255,
+                               names = c("Snö", "Fjäder", "Glimmer", "Betong",
+                                         "Titan", "Kol")))
   )
+  if (!color_names) {
+    names(pal) <- NULL
+  }
+  return(pal)
 }
 
-SLUpalette.numeric <- function(palette) {
+SLUpalette.numeric <- function(palette, color_names = TRUE) {
   if (!is.integer(palette) & (palette < 1 | palette > 5) ) {
     stop('Numeric palette must be an integer between 1 and 5')
   }
   pall <- SLUpalette.character("all")
-  pall[seq(from = palette, by = 5, length.out = 5)]
+  pal <- pall[seq(from = palette, by = 5, length.out = 5)]
+  if (!color_names) {
+    names(pal) <- NULL
+  }
+  return(pal)
 }
--- R/theme_SLU.R
+++ R/theme_SLU.R
@@ -1,22 +1,31 @@
+#' SLU theme for ggplot2
+#'
+#' @param palette
+#' @param dark
+#'
+#' @return
+#' @export
+#'
+#' @examples
 theme_SLU <- function(palette, dark = TRUE) {
   if (!requireNamespace("ggplot2", quietly = TRUE)) {
     stop("ggplot2 required for theme_SLU to work",   call. = FALSE)
   }
-  cols  <- SLUcolors::SLUpalette(palette)
-  greys  <- SLUcolors::SLUpalette("grey")
+  cols  <- SLUcolors::SLUpalette(palette, color_names = FALSE)
+  greys  <- SLUcolors::SLUpalette("grey", color_names = FALSE)
   if (dark) {
     cols  <- rev(cols)
     greys  <- rev(greys)
   }
-  theme(rect  = element_rect(fill = cols[5]),
-        text = element_text(color = greys[2]),
-        plot.title = element_text(hjust = 0.5), #argin = margin(b = -3), size = 9.6, face = "bold"),
-        axis.text = element_text(color = greys[2]),
-        axis.ticks = element_line(color = greys[2]),
-        panel.grid.minor = element_line(color = NA),
-        panel.border = element_rect(color = greys[1], fill = NA),
-        panel.background = element_blank(), #element_rect(fill = cols[5]),
-        panel.grid = element_line(color = NA))
+  ggplot2::theme(rect  = ggplot2::element_rect(fill = cols[5]),
+        text = ggplot2::element_text(color = greys[2]),
+        plot.title = ggplot2::element_text(hjust = 0.5), #argin = margin(b = -3), size = 9.6, face = "bold"),
+        axis.text = ggplot2::element_text(color = greys[2]),
+        axis.ticks = ggplot2::element_line(color = greys[2]),
+        panel.grid.minor = ggplot2::element_line(color = NA),
+        panel.border = ggplot2::element_rect(color = greys[1], fill = NA),
+        panel.background = ggplot2::element_blank(), #ggplot2::element_rect(fill = cols[5]),
+        panel.grid = ggplot2::element_line(color = NA))
 }
 
 #p1 <- ggplot(iris, mapping = aes(x = Sepal.Width, y = Sepal.Length)) +  labs(title = "Sepal plot") + geom_point()
