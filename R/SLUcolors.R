ae6539d1bf8b7eb372884ab44b0050b5ac4a111a
Anders Kagervall <Anders.Kagervall@slu.se>
2022-06-30 15:37:40 +0200

    Make SLU_palettes internal

--- R/SLUcolors.R
+++ R/SLUcolors.R
@@ -4,15 +4,16 @@
 
 #' Function to extract SLU colors as hex codes
 #'
-#' @param ... Character names of SLU_colors
+#' @param ... Character names of SLU_colors if NULL all colors from the official SLU color guide are returned.
 #'
+#' @export
 SLU_cols <- function(...) {
   cols <- c(...)
 
   if (is.null(cols))
-    return (SLUcolors::SLU_colors)
+    return (SLUcolors::SLU_colors[1:26]) # Return first 26 colors (the official SLU colors)
 
-  SLUcolors::SLU_colors[cols]
+  return(SLUcolors::SLU_colors[cols])
 }
 
 
@@ -32,7 +33,7 @@ SLU_pal.default <- function(palette, reverse = FALSE) {
 }
 #' @export
 SLU_pal.character <- function(palette = "all", reverse = FALSE, ...) {
-  res <- SLUcolors::SLU_palettes[[palette]]
+  res <- SLUcolors:::SLU_palettes[[palette]]
   if (reverse)
     res <- rev(res)
   grDevices::colorRampPalette(res, ...)
@@ -42,7 +43,7 @@ SLU_pal.numeric <- function(palette = "all", reverse = FALSE, ...) {
   if (!is.integer(palette) & (palette < 1 | palette > 5) ) {
     stop('Numeric palette must be an integer between 1 and 5')
   }
-  pall <- SLUcolors::SLU_palettes[["all"]]
+  pall <- SLUcolors:::SLU_palettes[["all"]]
   res <- pall[seq(from = palette, by = 5, length.out = 5)]
   if (reverse)
     res <- rev(res)
@@ -55,7 +56,8 @@ SLU_pal.numeric <- function(palette = "all", reverse = FALSE, ...) {
 #' \code{SLUpalette(palette)} returns different variants of the official SLU color palette.
 #'
 #'
-#' @param palette character or numeric defining the desired color palette
+#' @param palette character or numeric defining the desired color palette. Allowed values  "all", "red",
+#'  "green", "blue", "yellow" and "grey" or a number 1:5.
 #' @param color_names logical return named vector or not. Default TRUE
 #'
 #' @return a named vector of color specifications.
@@ -81,10 +83,10 @@ SLUpalette.default <- function(palette, color_names) {
 
 #' @export
 SLUpalette.character  <- function(palette, color_names = TRUE) {
-  if (!palette %in% names(SLU_palettes)) {
-    stop('Unknown color palette "',palette, '" must be one of ', paste(names(SLU_palettes), collape = " "))
+  if (!palette %in% names(SLUcolors:::SLU_palettes)) {
+    stop('Unknown color palette "',palette, '" must be one of ', paste(names(SLUcolors:::SLU_palettes), collape = " "))
   }
-  pal <- SLUcolors::SLU_palettes[[palette]]
+  pal <- SLUcolors:::SLU_palettes[[palette]]
   if (!color_names) {
     names(pal) <- NULL
   }
@@ -97,7 +99,7 @@ SLUpalette.numeric <- function(palette, color_names = TRUE) {
     stop('Numeric palette must be an integer between 1 and 5')
   }
 #  pall <- SLUpalette.character("all")
-  pall <- SLUcolors::SLU_palettes[["all"]]
+  pall <- SLUcolors:::SLU_palettes[["all"]]
   pal <- pall[seq(from = palette, by = 5, length.out = 5)]
   if (!color_names) {
     names(pal) <- NULL
